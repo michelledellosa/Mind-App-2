f060aec57a85e3cf9af353cf4b0f1da781017b71
Michelle Dellosa <michelledellosa7@gmail.com>
2022-01-15 21:01:08 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'testAngular',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
